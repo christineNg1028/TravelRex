9045403bc2a4b22380f6cc7f503a22409db22654
Tara Rao-Pandit <75340062+traopand@users.noreply.github.com>
2024-03-18 22:21:18 +0100

    add footer + fragments

--- app/src/main/java/es/uc3m/android/travel_rex/MainActivity.java
+++ app/src/main/java/es/uc3m/android/travel_rex/MainActivity.java
@@ -1,14 +1,69 @@
 package es.uc3m.android.travel_rex;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import es.uc3m.android.travel_rex.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        //setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            int id = item.getItemId();
+            if (id == R.id.home) {
+                replaceFragment(new HomeFragment());
+            }
+            else if (id == R.id.map) {
+                replaceFragment(new MapFragment());
+            }
+            else if (id == R.id.add) {
+                replaceFragment(new AddFragment());
+            }
+            else if (id == R.id.profile) {
+                replaceFragment(new ProfileFragment());
+            }
+
+
+            /*switch (item.getItemId()){
+
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.map:
+                    replaceFragment(new MapFragment());
+                    break;
+                case R.id.add:
+                    replaceFragment(new AddFragment());
+                    break;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+            }*/
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
+
     }
 }
