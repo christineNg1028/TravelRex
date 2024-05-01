118190c04e7022fb2b71643c331376acdc68dccb
Tara Rao-Pandit <75340062+traopand@users.noreply.github.com>
2024-05-01 23:45:16 +0200

    Add horizontal recycler views to profile page

---
+++ app/src/main/java/es/uc3m/android/travel_rex/WantedAdapter.java
@@ -0,0 +1,47 @@
+package es.uc3m.android.travel_rex;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+import es.uc3m.android.travel_rex.R;
+
+public class WantedAdapter extends RecyclerView.Adapter<WantedAdapter.ViewHolder> {
+
+    private List<String> dataList;
+
+    public WantedAdapter(List<String> dataList) {
+        this.dataList = dataList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.place_card, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String data = dataList.get(position);
+        holder.textView.setText(data);
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.place_name);
+        }
+    }
+}
